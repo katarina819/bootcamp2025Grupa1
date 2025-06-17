92efc1cbda73528417c3a2a02f5dc7cc0c42e1b8
LAPTOP-LRIPRM2Q\patri <patriciapruzinac1@gmail.com>
2025-06-17 09:10:43 +0200

    Added Domain models for Director and MovieDirector

---
+++ MoviesApp/MoviesApp.Model/Director.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.Model
+{
+    public class Director
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required.")]
+        [StringLength(100, ErrorMessage = "FirstName cannot exceed 100 characters.")]
+        public required string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        [StringLength(100, ErrorMessage = "LastName cannot exceed 100 characters.")]
+        public required string LastName { get; set; }
+
+    }
+}
---
+++ MoviesApp/MoviesApp.Model/MovieDirector.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.Model
+{
+    public class MovieDirector
+    {
+        [Key]
+        public Guid MovieId { get; set; }
+
+        [Key]
+        public Guid DirectorId { get; set; }
+    }
+}
