7753adc243072aade93fe90903d18f7695499a2b
ana-puskaric <ana.puskaric99@gmail.com>
2025-06-17 10:40:56 +0200

    Autofac

--- MoviesApp/MoviesApp.WebApi/Program.cs
+++ MoviesApp/MoviesApp.WebApi/Program.cs
@@ -1,14 +1,38 @@
+using Autofac;
+using Autofac.Core;
+using Autofac.Extensions.DependencyInjection;
+using MoviesApp.Repository;
+using MoviesApp.Repository.Common;
+using MoviesApp.Service;
+using MoviesApp.Service.Common;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-// Add services to the container.
-
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.Register(c => new NpgsqlConnection(connectionString))
+        .AsSelf()
+        .InstancePerLifetimeScope();
+    containerBuilder.RegisterType<MovieService>().As<IMovieService>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<MovieRepository>().As<IMovieRepository>().InstancePerLifetimeScope();
+    containerBuilder.RegisterAssemblyTypes(typeof(MovieRepository).Assembly)
+        .Where(t => t.Name.EndsWith("Repository"))
+        .AsImplementedInterfaces()
+        .InstancePerLifetimeScope();
+});
+
+// Add services to the container.
+
+
 
 var app = builder.Build();
 
