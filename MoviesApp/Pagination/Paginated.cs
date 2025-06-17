a952d3d0bf646c96f5cb65521ebe32f1b5c9292e
LAPTOP-LRIPRM2Q\patri <patriciapruzinac1@gmail.com>
2025-06-17 10:29:22 +0200

    Pagination

---
+++ MoviesApp/Pagination/Paginated.cs
@@ -0,0 +1,13 @@
+﻿namespace Pagination
+{
+    public class Paginated
+    {
+        // public List<T> Items { get; set; }   // list of items on current page
+        public int TotalCount { get; set; }  // total number of item in full collection
+        public int Page { get; set; }        // page number
+        public int PageSize { get; set; }    // number of items per page
+        
+    }
+
+}
+
